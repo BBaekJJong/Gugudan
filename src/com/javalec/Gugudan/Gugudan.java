ccabe3a0ce9ddc5686a298f0a31c1a882333ba03
Administrator <Administrator@43024B01_C0C>
2017-09-23 13:15:28 +0900

    0번 구구단 출력 변화

--- src/com/javalec/Gugudan/Gugudan.java
+++ src/com/javalec/Gugudan/Gugudan.java
@@ -12,12 +12,20 @@ public class Gugudan {
 	
 		if (number == 0) {
 			
+			for (int i = 1; i < 10; i++) {
+				System.out.print(i + "´Ü\t");
+			}
+	
+			System.out.println();
 			for (int i = 1; i < 9; i++) {
-				System.out.println(i + "´Ü");
-				for (int j = 1; j < 9; j++) {
-					System.out.print(i + "*" + j + "=");
-					System.out.println(i * j);
+				for (int j = 1; j < 10; j++) {
+					System.out.print(j + "*" + i + "=");
+					System.out.print(j*i+"\t");
+					
+//					System.out.print(i + "*" + j + "=");
+//					System.out.println(i * j);
 				}
+				System.out.println();
 			} 
 			
 		} else if (number > 0 && number < 10) {
