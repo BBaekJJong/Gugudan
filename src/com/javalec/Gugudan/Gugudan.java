bd041be69bdfa1928ba0ab62ad738772a7854888
Administrator <Administrator@43024B01_C0C>
2017-09-23 11:33:40 +0900

    code -

--- src/com/javalec/Gugudan/Gugudan.java
+++ src/com/javalec/Gugudan/Gugudan.java
@@ -58,20 +58,26 @@ public class Gugudan {
 		Scanner scanner = new Scanner(System.in);
 		int number = scanner.nextInt();
 		
-		for (int i = 1; i < 9; i++) {
-			System.out.println(number * i);
-		}
+//		for (int i = 1; i < 9; i++) {
+//			System.out.println(number * i);
+//		}
 		
 		if (number == 0) {
 			
 			for (int i = 1; i < 9; i++) {
 				System.out.println(i + "´Ü");
-			for (int j = 1; j < 9; j++) {
-				System.out.print(i + "*" + j + "=");
-				System.out.println(i * j);
-			}
+				for (int j = 1; j < 9; j++) {
+					System.out.print(i + "*" + j + "=");
+					System.out.println(i * j);
+				}
 		} 
 			
+		} else {
+			for (int i = 1; i < 9; i++) {
+				System.out.println(number + "´Ü");
+				System.out.print(number + "*" + i + "=");
+				System.out.print(number * i);
+			}
 		}
 		
 	}
